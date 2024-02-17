df1cef15529b0b984055953c7188fa061d38f7f9
juniordurval <160273751+juniordurval@users.noreply.github.com>
2024-02-17 12:06:49 -0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -72,6 +72,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
         // Enviamos el formulario
         document.getElementById('formulario').submit();
     }
+
+    // Funciones para el modal
+    var modal = document.getElementById("miModal");
+    var span = document.getElementsByClassName("cerrar")[0];
+
+    window.onload = function() {
+      modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
 });
+
 
   
